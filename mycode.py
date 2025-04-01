12bab65362dd4560ebf6eb3f5a959341c75397ca
mike.sawada <msawada@uottawa.ca>
2025-04-01 16:58:38 -0400

    integrated function module into main branch of mycode so it uses the function rather than repeating the code in the main

--- mycode.py
+++ mycode.py
@@ -1,7 +1,8 @@
 import arcpy
 from arcpy import env
+import myfunctionmodule
 env.workspace=r"c:\temp\OTTAWA.gdb\DATA"
 
 fclist= arcpy.ListFeatureClasses()
 for fc in fclist:
-    print(len(arcpy.ListFields(fc)))
+    print(myfunctionmodule.get_number_of_fields(fc))
