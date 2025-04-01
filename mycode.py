e4433431940209d63e75e90ccf72ee03d7b2eea7
mike.sawada <msawada@uottawa.ca>
2025-04-01 16:24:09 -0400

    added feature to list the number of fields in each feature class

--- mycode.py
+++ mycode.py
@@ -1,4 +1,7 @@
 import arcpy
 from arcpy import env
 env.workspace=r"c:\temp\OTTAWA.gdb\DATA"
-arcpy.ListFeatureClasses()
+
+fclist= arcpy.ListFeatureClasses()
+for fc in fclist:
+    print(len(arcpy.ListFields(fc)))
