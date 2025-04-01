8927c24556f709fa2187523e1ec6863c697dde91
mike.sawada <msawada@uottawa.ca>
2025-04-01 17:32:00 -0400

    sums the count of all fields across all feature classes

--- mycode.py
+++ mycode.py
@@ -4,5 +4,8 @@ import myfunctionmodule
 env.workspace=r"c:\temp\OTTAWA.gdb\DATA"
 
 fclist= arcpy.ListFeatureClasses()
+#Add a variable to hold a sum
+sum_field_lengths=0
 for fc in fclist:
-    print(myfunctionmodule.get_number_of_fields(fc))
+    # Add to sum of fields current nmber of fields
+    sum_field_lengths+=myfunctionmodule.get_number_of_fields(fc)
